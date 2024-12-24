88f2b66c1d690acd29a99f11b43f34d48dee3d41
unknown <hovietanh147@gmail.com>
2024-12-25 02:07:32 +0700

    change deloy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       onwarn(warning, warn) {
         // Bỏ qua một số cảnh báo nếu cần
         if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
       }
     }
+  },
+  server: {
+    host: true
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
   }
 })
