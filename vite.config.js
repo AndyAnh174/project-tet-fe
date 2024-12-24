e04ff4fee12a62f083ccce77b088c0a38a68fb6f
unknown <hovietanh147@gmail.com>
2024-12-25 01:52:16 +0700

    hotfix: deloy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Bỏ qua một số cảnh báo nếu cần
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
+      }
+    }
+  }
 })
