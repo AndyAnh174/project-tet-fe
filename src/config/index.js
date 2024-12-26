fd2aec432406a847bd5f81a7c75115a994efe3d7
unknown <hovietanh147@gmail.com>
2024-12-26 14:26:47 +0700

    add chat

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,1 @@
-const configs = {
-  development: {
-    API_URL: import.meta.env.VITE_API_URL || 'https://whole-downloading-bugs-denver.trycloudflare.com',
-  },
-  production: {
-    API_URL: import.meta.env.VITE_API_URL || 'https://whole-downloading-bugs-denver.trycloudflare.com',
-  },
-  staging: {
-    API_URL: import.meta.env.VITE_API_URL || 'https://staging-api.your-domain.com/api',
-  }
-};
-
-const env = import.meta.env.MODE || 'development';
-export default configs[env]; 
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5173'; 
